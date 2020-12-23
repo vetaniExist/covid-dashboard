40b1f1fe34999b4a3b2a437287f6ebf776b8c318
Huriosity <vetani3123@mail.ru>
2020-12-23 20:31:53 +0300

    feat: add map

--- src/js/layout/DashboardMap.js
+++ src/js/layout/DashboardMap.js
@@ -1,9 +1,24 @@
 import { createElement as createEl } from "../utils/elementsUtils";
+import L from '../../../node_modules/leaflet/dist/leaflet';
+import "../../../node_modules/leaflet/dist/leaflet.css";
 
 export class DashboardMap {
   constructor(parentNode) {
     console.log("MapLayout not impl");
     this.map = createEl("div", "covid_map", parentNode);
+    this.constructMap();
+  }
+
+  async constructMap() {
+    // this.leafletMapContainer = createEl()
+    await this.map.setAttribute("id", "mapid");
+    const mapOptions = {
+      center: [17.385044, 78.486671],
+      zoom: 10
+    }
+    var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+    var layer = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
+    mymap.addLayer(layer);
   }
 }
 
