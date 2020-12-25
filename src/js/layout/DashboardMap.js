c8fcc93e8d554aa7cdc6dda30a07542404a4898d
Huriosity <vetani3123@mail.ru>
2020-12-25 04:06:09 +0300

    feat: add  current mode in map-popup; add diff beetwen total cases

--- src/js/layout/DashboardMap.js
+++ src/js/layout/DashboardMap.js
@@ -3,44 +3,35 @@ import L from "../../../node_modules/leaflet/dist/leaflet";
 import "../../../node_modules/leaflet/dist/leaflet.css";
 
 import { CovidData as CData } from "../entries/covidData";
-
-import { nominatimOSMRequest, getGeoJSON } from "../utils/mapAPIUtils"
-import { geoJSON } from "leaflet";
-
-import iconP from "../../../node_modules/leaflet/dist/images/marker-icon.png";
-import shadow from "../../../node_modules/leaflet/dist/images/marker-shadow.png";
-import { icon, Marker } from 'leaflet';
+import { getGeoJSON } from "../utils/mapAPIUtils";
 
 
 
 export class DashboardMap {
-  constructor(parentNode) {
+  constructor(parentNode, datalinked) {
     console.log("MapLayout not impl");
     this.map = createEl("div", "covid_map", parentNode);
-    this.constructMap();
+    this.constructMap(datalinked);
     this.data = new CData();
-    // this.popupDiv = createEl("div", "display-none flex popup_div", this.map);
   }
 
-  async constructMap() {
+  async constructMap(datalinked) {
     this.leafletMapContainer = createEl("div", "covid_map-container", this.map)
     await this.leafletMapContainer.setAttribute("id", "mapid");
+    this.countries = await this.data.getAllCountries();
+    console.log("страны");
+    console.log(this.countries);
     const mapOptions = {
       center: [17.385044, 78.486671],
       zoom: 1,
       maxZoom: 10,
       // minZoom: 1,
     };
-    const mymap = L.map("mapid", mapOptions);// setView([51.505, -0.09], 13);
-    const layer = new L.TileLayer("http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png");
-    mymap.addLayer(layer);
+    const mymap = L.map("mapid", mapOptions);
+    new L.TileLayer("http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(mymap);
+
     let geoJSON = await getGeoJSON();
 
-
-    // const arrOfCountrysCenter = [];
-    const afgan = geoJSON.features[0];
-    console.log(afgan);
-    console.log(afgan.geometry.coordinates[0].slice());
     geoJSON.features.forEach((country, ind) => {
       if (country.geometry.type === "Polygon") {
         country.geometry.coordinates.forEach((polygon) => {
@@ -51,35 +42,56 @@ export class DashboardMap {
       } else if (country.geometry.type === "MultiPolygon") {
         country.geometry.coordinates.forEach((polygons) => {
           polygons.forEach((polygon) => {
-            // console.log(polygon);
             polygon.forEach((el, index) => {
-              // console.log(index, el);
               polygon[index] = [el[1], el[0]];
             });
           });
         });
       }
+      const countryObj = this.countries.filter((el) => el.name === country.properties.name)[0];
+      // console.log(countryObj.todayConfirmed, countryObj.name);
       const polygon = L.polygon(country.geometry.coordinates, country.properties).addTo(mymap);
       const center = polygon.getBounds().getCenter();
+      const setting = this.getSetting(countryObj);
       L.circle(center, {
-        color: "red",
+        color: setting[1],
         fillColor: "#f03",
         fillOpacity: 0.5,
-        radius: 50.0,
-      })
-        .addTo(mymap);
-      polygon.on("mouseout", () => {
-      });
+        radius: setting[0],
+      }).addTo(mymap);
+
       polygon.on("mouseover", (e) => {
         console.log(country.properties.name);
         console.log(e.latlng)
         console.log(center);
-        L.popup().setLatLng(center).setContent(`<p>${country.properties.name}<br/></p>`).openOn(mymap);
-        // this.popupDiv.textContent = country.properties.name;
-        // this.popupDiv.classList.remove("display-nonw");
-        // this.map.appendChild(this.popupDiv);
+        L.popup().setLatLng(center).setContent(`<p>${country.properties.name}<br/>${datalinked.getcontrolPanelDataText()}</p>`).openOn(mymap);
       });
     });
+  }
+
+  getSetting(country) {
+    if (!country) {
+      console.log("return: ", 10);
+      return [10.0, "white"];
+    }
+    if (country.totalConfirmed > 5000) {
+      console.log("return: ", 30);
+      return [50.0, "red"];
+    }
+    if (country.totalConfirmed > 1000) {
+      console.log("return: ", 40);
+      return [70.0, "purple"];
+    }
+    if (country.totalConfirmed > 500000) {
+      console.log("return: ", 50);
+      return [90.0, "darkgreen"];
+    }
+    if (country.totalConfirmed > 5000000) {
+      console.log("return: ", 60);
+      return [120.0,"black"];
+    }
+    console.log("return: ", 20);
+    return [20.0, "gren"];
   }
 }
 
